c3085808e5fe255f84788479c3bdd3ee7ab8c964
gerome-byte <geromeaidan@gail.com>
2022-09-09 06:05:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * description: printing to the standard erorr
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		write(2, str1, 59);
+		return (1);
+}
