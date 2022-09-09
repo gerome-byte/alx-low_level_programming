20c9db1d2d4f0bd15818a137d9c404a64d47861d
gerome-byte <geromeaidan@gail.com>
2022-09-09 04:44:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- *
  *  * main - Entry point
  *
  *   *
  *
  *    * Return: Always 0 (Success)
- *
  *     */
 
 int main(void)
@@ -15,5 +13,4 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 		return (0);
-
 }
