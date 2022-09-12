1c51535dd0c800b4d95430321afc981e934a6acd
gerome-byte <geromeaidan@gail.com>
2022-09-12 07:02:20 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *description: print alphabet
+ *
+ *
+ * Return: alway returns 0
+ */
+
+int main(void)
+
+{
+
+		char a;
+
+
+
+			for (a = 'a'; a <= 'z' ; a++)
+
+			putchar(a);
+
+			for (a = 'A'; a <= 'Z'; a++)
+
+			putchar(a);
+
+			putchar('\n');
+
+			return (0);
+}
