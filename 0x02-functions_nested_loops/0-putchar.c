0596d2a1c84c880843eba03b3cde3a018e67193c
gerome-byte <geromeaidan@gail.com>
2022-09-13 11:48:41 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints _putchar
+ *
+ * return: always returns 0
+ */
+
+int main(void)
+
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return(0);
