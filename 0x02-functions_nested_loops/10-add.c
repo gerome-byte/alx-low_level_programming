68303e52b3f60bc42d5065c9ac9e79d214199ffd
gerome-byte <geromeaidan@gail.com>
2022-09-14 12:00:42 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two numbers from input
+ *
+ *
+ * Description: adds two numbers
+ *
+ * Return: Always (0).
+ */
+
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
