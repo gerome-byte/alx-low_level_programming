f08c8de9adeca3d9fe3a7ecff764cf590ca43b88
gerome-byte <geromeaidan@gail.com>
2022-09-21 12:09:02 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * char *_strcat - concatenates two strings
+ * @str1[] - first string
+ * @str2[] - second string
+ * ptr - pointer
+ * return 0
+ */
+
+void char *_strcat(char *dest, char *src)
+
+{
+	char str1[];
+	char str2[];
+	char *ptr;
+
+	ptr = _strcat(str1, str2);
+
+	return = (0);
+}
