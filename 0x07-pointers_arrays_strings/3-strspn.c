aef928961f66a6ebcae1127c89d3d437de0151a1
gerome-byte <geromeaidan@gail.com>
2022-09-27 11:15:39 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - seraches a string fir a set of bytes
+ * @s: string to be searched.
+ * @accept: concists of bytes to be compared with the string s.
+ * Return: the number of bytes in the intitial segment s similar to those from
+ * accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				count++;
+		break;
+		}
+	}
+	if s[j] == ('\0')
+	return (count);
+}
