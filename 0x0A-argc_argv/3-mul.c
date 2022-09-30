d604de9de7e3cbd6e70b9912d22a8f5eed05b4aa
gerome-byte <geromeaidan@gail.com>
2022-09-30 12:54:24 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - mulitiplies two numbers
+ * @argc: counts arguments
+ * @argv: array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	if (argc == 3)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
+}
