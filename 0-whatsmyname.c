85f86087cdb67b28a12b20ac9de8a426e0b21cf2
gerome-byte <geromeaidan@gail.com>
2022-09-30 08:06:46 -0700

    0-whatsmyname.c

---
+++ 0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - prints the programs name
+ * @argc: argument count
+ * @argv: array of strings
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+			printf("argv[%d] = %s\n", i, argv[i]);
+	}
+		return (0);
+}
