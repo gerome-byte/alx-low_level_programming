054c9dddb4a033a4528242670294efd77cc68a2b
gerome-byte <geromeaidan@gail.com>
2022-10-03 09:47:46 -0700

    libmy.a

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of the substring
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return ('\0');
+}
