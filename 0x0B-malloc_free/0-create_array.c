86a40fe85cbfe1bf1e466be7fc523a93432b9a6a
gerome-byte <geromeaidan@gail.com>
2022-10-08 01:22:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,16 @@
 char *create_array(unsigned int size, char c)
 {
 	char *str;
+	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
-	str[size] = c;
+
 
 	if (size == 0)
 		return (NULL);
 	if (str == NULL)
 		return (NULL);
-	else
+	for (i = 0; i < size; i++)
+		str[i] = c;
 		return (str);
 }
