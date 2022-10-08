b507abc6d05ce08ce786bd702edc53e54738cd39
gerome-byte <geromeaidan@gail.com>
2022-10-08 11:34:30 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,12 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *str;
-	int i, j, k;
+	int i, j, k, l;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	i = 0;
 	j = 0;
@@ -18,22 +23,19 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 		i++;
 	while (s2[j] != '\0')
-	{
-		s1[i] = s2[j];
 		j++;
-		i++;
-	}
-	s1[i] = ('\0');
 
-	return (s1);
-
-	str = malloc(sizeof(char) * i);
+	str = malloc(sizeof(char) * i + j + 1);
 
 	if (str == NULL)
 		return (NULL);
 
 	for (k = 0; k < i; k++)
 		str[k] = s1[k];
+	l = j;
+
+	for (j = 0; j < l; j++)
+		str[k] = s2[j];
 
 	return (str);
 }
