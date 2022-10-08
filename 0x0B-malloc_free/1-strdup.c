d954aa8504c3504b662b7a717a931770df37d32a
gerome-byte <geromeaidan@gail.com>
2022-10-08 04:04:23 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,11 +9,16 @@
 char *_strdup(char *str)
 {
 	char *ptr;
-	unsigned int i;
+	unsigned int i, j;
 
-	ptr = malloc(sizeof(*str));
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		ptr = malloc(sizeof(char) * j + 1);
+	}
 
 	if (str == NULL)
+		return (NULL);
+	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		ptr[i] = str[i];
