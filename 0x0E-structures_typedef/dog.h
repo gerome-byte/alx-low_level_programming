3f915137bd9ccc22dacdb90bae01105f35b3fbac
gerome-byte <geromeaidan@gail.com>
2022-10-12 22:38:59 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef"dog.h"
+#define"dog.h"
+
+/**
+ * struct dog - structure to define elements of a dog.
+ * @name: first member
+ * @age: second element
+ * @owner: third element
+ *
+ * Description: decribing elements of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
