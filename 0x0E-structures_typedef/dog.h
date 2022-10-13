4856fb90888116c19b603d086caed4aa9e882cc6
gerome-byte <geromeaidan@gail.com>
2022-10-13 00:55:47 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _DOG_H_
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure to define elements of a dog.
  * @name: first member
  * @age: second element
