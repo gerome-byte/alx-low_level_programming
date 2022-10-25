3f2fc4c0759b06197cd27bc0086290b472d8975c
gerome-byte <geromeaidan@gail.com>
2022-10-25 03:56:26 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
-	
+
 	if (h == NULL)
 		printf("error\n");
-	
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		n++;
-		
 	}
 	return (n);
 }
