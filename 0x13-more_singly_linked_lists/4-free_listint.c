9a8f26bded7f722ac022313e67e6a3bc1c265293
gerome-byte <geromeaidan@gail.com>
2022-10-28 05:04:01 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees list
+ * @head: first pointer.
+ * Return: nothing.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	while ((ptr = head) != NULL)
+	{
+		head = head->next;
+		free(ptr->next);
+		free(ptr);
+	}
