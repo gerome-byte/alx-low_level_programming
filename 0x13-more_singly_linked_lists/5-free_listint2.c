37ca14900eee0d33f8401c89a102e822d4da5571
gerome-byte <geromeaidan@gail.com>
2022-10-28 11:39:12 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a list.
+ * @head: first pointer
+ * Return: nothing.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *xtr;
+
+	xtr = *head;
+
+	while ((ptr = xtr) != NULL)
+	{
+		xtr = xtr->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
