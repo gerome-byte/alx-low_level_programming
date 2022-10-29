33364a8733af0cb8f5e13eb846139b09641e28ff
gerome-byte <geromeaidan@gail.com>
2022-10-28 22:32:14 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+
+/**
+ * sum_listint - sums up all the node data.
+ * @head: head pointer.
+ * Return: the sum of all the node data.
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *ptr;
+	size_t k = 0;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		k += ptr->data;
+		ptr = ptr->next;
+	}
+	return (k);
+}
