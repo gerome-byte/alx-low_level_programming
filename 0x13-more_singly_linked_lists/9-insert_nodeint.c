480adf6f99465eda29bdd81c56b6ff9d9cc9067c
gerome-byte <geromeaidan@gail.com>
2022-10-29 02:05:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,9 +21,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	xtr = *head;
 	if (idx != 0)
 	{
-	for (i = 0; i < idx - 1; i++)
+	for (i = 0; i < idx - 1 && xtr != NULL; i++)
 		xtr = xtr->next;
 	}
+	if (xtr == NULL && idx != 0)
+		return (NULL);
 	if (idx == 0)
 	{
 		ptr->next = *head;
